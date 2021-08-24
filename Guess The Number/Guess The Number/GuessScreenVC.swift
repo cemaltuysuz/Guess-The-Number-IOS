7b3cfe8c7558c4b55d14121abb76110768e73883
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-24 19:05:28 +0300

    finished

--- Guess The Number/Guess The Number/GuessScreenVC.swift
+++ Guess The Number/Guess The Number/GuessScreenVC.swift
@@ -9,8 +9,57 @@ import UIKit
 
 class GuessScreenVC: UIViewController {
 
+    @IBOutlet weak var remainingTrial: UILabel!
+    @IBOutlet weak var router: UILabel!
+    @IBOutlet weak var inputNum: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    var randomNum:Int?
+    var count:Int = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        randomNum  = Int.random(in: 0...100) // random generate
+        print("Random number is : \(randomNum!)")
+    }
+    @IBAction func enterButton(_ sender: Any) {
+        if let incomingNum = inputNum.text{
+            if !incomingNum.isEmpty {
+                if let convertNum = Int(incomingNum){
+                    count-=1
+                    if randomNum == convertNum {
+                        performSegue(withIdentifier: "guessToResult", sender: true)
+                    }else if convertNum>randomNum!{
+                        router.text = "decrease"
+                    }
+                    else if convertNum<randomNum!{
+                        router.text = "increase"
+                    }
+                    router.isHidden = false
+                    if count == 0 {
+                        performSegue(withIdentifier: "guessToResult", sender: false)
+                    }
+                    remainingTrial.text = "Remaining trial : \(count)"
+                }else{
+                    errorShow(status: false,message: "Please, enter numeric type value.")
+                }
+            }
+            else{
+                errorShow(status: false, message: "can not enter empty value.")
+            }
+        }
+    }
+    
+    func errorShow(status:Bool,message:String){
+        errorLabel.isHidden = status
+        errorLabel.text = message
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let incomingData = sender as? Bool{
+            let targetVC = segue.destination as! ResultScreenVT
+            targetVC.isSuccess = incomingData
+            targetVC.rndNum = randomNum!
+        }
     }
 }
--- Guess The Number/Guess The Number/ResultScreenVT.swift
+++ Guess The Number/Guess The Number/ResultScreenVT.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class ResultScreenVT: UIViewController {
 
+    @IBOutlet weak var `case`: UILabel!
+    @IBOutlet weak var caseImage: UIImageView!
+    @IBOutlet weak var num: UILabel!
+    var isSuccess:Bool?
+    var rndNum:Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let image2 = UIImage(named: "Sad Face")
 
-        // Do any additional setup after loading the view.
+        if isSuccess != nil {
+            if isSuccess! {
+                `case`.text = "Win"
+            }else{
+                `case`.text = "Lose"
+                caseImage.image = image2
+            }
+            num.text = "number is : \(rndNum!)"
+        }else{
+            `case`.text = "Something went wrong..."
+        }
+        
     }
-
+    @IBAction func retryGame(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
 }

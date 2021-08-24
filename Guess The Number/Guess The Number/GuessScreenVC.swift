69e0df889a9d872f24b38ae4e27f15188b6feaef
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-24 17:16:55 +0300

    created ; Project | ViewControllers | Classes

---
+++ Guess The Number/Guess The Number/GuessScreenVC.swift
@@ -0,0 +1,16 @@
+//
+//  GuessScreenVC.swift
+//  Guess The Number
+//
+//  Created by cemal tüysüz on 24.08.2021.
+//
+
+import UIKit
+
+class GuessScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Guess The Number/Guess The Number/ResultScreenVT.swift
@@ -0,0 +1,29 @@
+//
+//  ResultScreenVT.swift
+//  Guess The Number
+//
+//  Created by cemal tüysüz on 24.08.2021.
+//
+
+import UIKit
+
+class ResultScreenVT: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
